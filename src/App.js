78eb7e7ad266ce87771b9b31189ba7d04b44cd68
talat <talathussain151214@gmail.com>
2022-10-29 16:19:28 +0500

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [cal, setCal] = useState('');
+  const [result, setResult] = useState('');
+  const opts = ['+', '-', '*', '/', '.'];
+
+
+  // the && operator only returns true when both of its operands
+  // are true (and false in all other cases), while the || operator
+  // only returns false when both of its operands are false (and true in all other cases).
+
+  const updatecal = (value) => {
+    if ((opts.includes(value) && cal === '') ||
+      (opts.includes(value) && opts.includes(cal.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCal(cal + value)
+
+    if (!opts.includes(value)) {
+      setResult((eval(cal + value).toString()));
+    }
+  }
+
+  const equal = () => {
+    setCal(eval(cal).toString());
+  }
+
+  const singledigitremove = () => {
+    if (cal == ' ') {
+      return;
+    }
+    const value = cal.slice(0, -1);
+    setCal(value);
+  }
+
+  const removeall = () => {
+    setCal(" ") || setResult("0");
+  }
+
+  function Createdegit() {
+    const degit = [];
+    for (let i = 1; i < 10; i++) {
+      degit.push(
+        <button onClick={() => updatecal(i.toString())} key={i}>{i}</button>
+      )
+    }
+    return degit;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calculator'>
+        <div className='displayinput'>
+          {result ? <span>({result})</span> : ''} {cal || "0"}
+        </div>
+        <div className='operator'>
+          <button onClick={() => updatecal('+')}>+</button>
+          <button onClick={() => updatecal('-')}>-</button>
+          <button onClick={() => updatecal('*')}>*</button>
+          <button onClick={() => updatecal('/')}>/</button>
+          <button onClick={singledigitremove}>AC</button>
+          <button onClick={removeall}>CE</button>
+        </div>
+        <div className='otherdigit'>
+          {Createdegit()}
+          <button onClick={() => updatecal('0')}>0</button>
+          <button onClick={() => updatecal('.')}>.</button>
+          <button onClick={equal}>=</button>
+        </div>
+
+      </div>
     </div>
   );
 }
